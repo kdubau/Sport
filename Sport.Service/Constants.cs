f5a75aa697ae760db6a336aec32e18431392285a
kdubau <kyle.white@live.com>
2018-02-06 07:53:45 -0500

    Stand up XQA backend services
    And make changes to point to them.
    
    Resource.designer.cs update
    
    Update Bundle ID and Package ID
    
    Prepare mobile apps with new back end and scripts for app center
    
    move app center pre build script
    
    Fix pre build scripts
    
    Fix pre build scripts to work on macOS
    
    update GCM sender id

--- Sport.Service/Constants.cs
+++ Sport.Service/Constants.cs
@@ -1,9 +1,12 @@
 ﻿
+using System.Configuration;
+
 namespace Sport.Service
 {
 	internal class Constants
 	{
-		internal static readonly string HubConnectionString = "Endpoint=sb://xamarinsporthubnamespace.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=TF6GC3POPu3WnxZxKdv520ITMZ3rh7Lmj58STHpMuow=";
-		internal static readonly string HubName = "XamarinSportNotificationHub";
-	}
+		internal static readonly string HubConnectionString = ConfigurationManager.ConnectionStrings["MS_NotificationHubConnectionString"].ConnectionString;
+		internal static readonly string HubName = ConfigurationManager.AppSettings["MS_NotificationHubName"];
+
+    }
 }
