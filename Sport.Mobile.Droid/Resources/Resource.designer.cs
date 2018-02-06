2b0324d2bc38476658753d9537b49f5e14bba29c
kdubau <kyle.white@live.com>
2018-02-06 07:58:18 -0500

    Resource.designer.cs update

--- Sport.Mobile.Droid/Resources/Resource.designer.cs
+++ Sport.Mobile.Droid/Resources/Resource.designer.cs
@@ -7319,25 +7319,8 @@ namespace Sport.Mobile.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f02013f
-			public const int avd_hide_password_1 = 2130837823;
-			
-			public const int avd_hide_password_2 = 2130837824;
-			
-			// aapt resource value: 0x7f020141
-			public const int avd_hide_password_3 = 2130837825;
-			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
-			
-			// aapt resource value: 0x7f020142
-			public const int avd_show_password_1 = 2130837826;
-			
-			// aapt resource value: 0x7f020143
-			public const int avd_show_password_2 = 2130837827;
-			
-			// aapt resource value: 0x7f020144
-			public const int avd_show_password_3 = 2130837828;
 			
 			// aapt resource value: 0x7f020056
 			public const int common_full_open_on_phone = 2130837590;
