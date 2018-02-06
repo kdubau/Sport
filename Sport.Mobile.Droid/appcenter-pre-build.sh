8e2d8e8469036acaf9ba08a2ee4c80bfe0357e56
kdubau <kyle.white@live.com>
2018-02-06 09:08:27 -0500

    Fix pre build scripts

--- Sport.Mobile.Droid/appcenter-pre-build.sh
+++ Sport.Mobile.Droid/appcenter-pre-build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [ -z "$APPCENTER_IOS_KEY" ];
+if [ ! -z "$APPCENTER_IOS_KEY" ];
 then
     sed -i "s/%APPCENTER_IOS_KEY%/$APPCENTER_IOS_KEY/g" $APPCENTER_SOURCE_DIRECTORY/Sport.Mobile.Shared/Keys.cs
 fi
 
-if [ -z "$APPCENTER_ANDROID_KEY" ];
+if [ ! -z "$APPCENTER_ANDROID_KEY" ];
 then
     sed -i "s/%APPCENTER_ANDROID_KEY%/$APPCENTER_ANDROID_KEY/g" $APPCENTER_SOURCE_DIRECTORY/Sport.Mobile.Shared/Keys.cs
 fi
